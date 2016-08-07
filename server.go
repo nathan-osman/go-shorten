0151f9a8340d4484f0854841f67b823a4869c0e1
Nathan Osman <nathan@quickmediasolutions.com>
2016-08-06 21:47:28 -0700

    Completed the remaining part of the initial implementation.

--- server.go
+++ server.go
@@ -4,18 +4,68 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/hectane/go-asyncserver"
 
+	"fmt"
+	"html/template"
 	"net/http"
 )
 
 // Server responds to HTTP requests and routes them accordingly.
 type Server struct {
 	server   *server.AsyncServer
+	config   *Config
 	database *Database
+	template *template.Template
+}
+
+// message represents a message to be displayed.
+type message struct {
+	Type string
+	Body string
 }
 
 // adminHandler processes requests for the admin page.
 func (s *Server) adminHandler(w http.ResponseWriter, r *http.Request) {
-	//...
+	messages := make([]message, 0)
+	defer func() {
+		s.template.Execute(w, map[string]interface{}{
+			"database": s.database,
+			"messages": messages,
+		})
+	}()
+	if r.Method == http.MethodPost {
+		if err := r.ParseForm(); err != nil {
+			messages = append(messages, message{"error", err.Error()})
+		} else {
+			switch r.Form.Get("action") {
+			case "new":
+				var (
+					path        = r.Form.Get("path")
+					destination = r.Form.Get("destination")
+				)
+				if len(path) == 0 || len(destination) == 0 {
+					messages = append(messages, message{"error", "'path' and 'destination' cannot be empty"})
+					return
+				}
+				s.database.Paths[path] = destination
+				messages = append(messages, message{"info", "New item added"})
+			case "delete":
+				path := r.Form.Get("path")
+				if len(path) == 0 {
+					messages = append(messages, message{"error", "'path' cannot be empty"})
+					return
+				}
+				delete(s.database.Paths, path)
+				messages = append(messages, message{"info", fmt.Sprintf("'%s' deleted", path)})
+			default:
+				messages = append(messages, message{"error", fmt.Sprintf("Unrecognized action '%s'", r.Form.Get("action"))})
+				return
+			}
+			if err := s.database.Save(); err != nil {
+				messages = append(messages, message{"error", err.Error()})
+				return
+			}
+		}
+	}
 }
 
 // NewServer creates a new HTTP server.
@@ -24,11 +74,17 @@ func NewServer(cfg *Config, db *Database) (*Server, error) {
 		r = mux.NewRouter()
 		s = &Server{
 			server:   server.New(cfg.Addr),
+			config:   cfg,
 			database: db,
+			template: template.New("admin"),
 		}
 	)
 	r.HandleFunc(cfg.AdminPath, s.adminHandler)
 	r.NotFoundHandler = s
+	s.server.Handler = r
+	if _, err := s.template.Parse(adminTemplate); err != nil {
+		return nil, err
+	}
 	if err := s.server.Start(); err != nil {
 		return nil, err
 	}
---
+++ static.go
@@ -0,0 +1,74 @@
+package main
+
+// Template for the admin page.
+const adminTemplate = `
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300|Roboto+Mono:300" rel="stylesheet" type="text/css">
+        <style>
+            body { font-family: "Roboto", sans; }
+            input[type=text] { font-family: "Roboto Mono" monospace; font-size: 12pt; margin: 8px 0; width: 256px; }
+            input[type=text]:focus { outline: none; }
+            label { display: block; margin: 16px 0; }
+            label input { display: block; border: none; border-bottom: 1px solid #ccc; }
+            table { border: 1px solid #ccc; border-collapse: collapse; width: 100%; }
+            tr:nth-child(odd) { background-color: #eee; }
+            th { background-color: #ddd; text-align: left; }
+            th, td { padding: 4px 8px; }
+            td.break { word-break: break-word; }
+            td.right { text-align: right; }
+            .message { padding: 4px 8px; }
+            .message.info { background-color: #def; border: 1px solid #5cf; }
+            .message.error { background-color: #fdd; border: 1px solid #f55; }
+            @media (min-width:768px) {
+                .container { margin: auto; width: 640px; }
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            {{ range $m := .messages }}
+                <div class="message {{ $m.Type }}">{{ $m.Body }}</div>
+            {{ end }}
+            <h1>go-shorten</h1>
+            <p>The redirects are displayed in two tables below.</p>
+            <table>
+                <tr>
+                    <th>Path</th>
+                    <th>Destination</th>
+                    <th></th>
+                </tr>
+                {{ range $k, $v := .database.Paths }}
+                    <tr>
+                        <td class="break">{{ $k }}</td>
+                        <td class="break">
+                            <a href="{{ $v }}" target="_blank">{{ $v }}</a>
+                        </td>
+                        <td class="right">
+                            <form method="post">
+                                <input type="hidden" name="action" value="delete">
+                                <input type="hidden" name="path" value="{{ $k }}">
+                                <button type="submit">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                {{ end }}
+            </table>
+            <h2>Add New</h2>
+            <form method="post">
+                <input type="hidden" name="action" value="new">
+                <label>
+                    Path: <input type="text" name="path">
+                </label>
+                <label>
+                    Destination: <input type="text" name="destination">
+                </label>
+                <button type="submit">Add</button>
+            </form>
+        </div>
+    </body>
+</html>
+`
